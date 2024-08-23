d71547b341bc42cb1410ab27703ec3b3acbc201d
ozne32 <enzorcc.mf3rs@gmail.com>
2024-08-23 18:39:33 -0300

    melhorando o sql

--- schemas/compras.sql
+++ schemas/compras.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: db
--- Tempo de geração: 23/08/2024 às 21:15
--- Versão do servidor: 11.4.2-MariaDB-ubu2404
--- Versão do PHP: 8.2.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `compras`
---
-
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `tb_produtos`
---
 
 CREATE TABLE `tb_produtos` (
   `produto_id` int(11) NOT NULL,
@@ -33,27 +6,10 @@ CREATE TABLE `tb_produtos` (
   `comprado` int(2) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `tb_produtos`
---
+-- Índices para tabela `tb_produtos`
 ALTER TABLE `tb_produtos`
   ADD PRIMARY KEY (`produto_id`);
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `tb_produtos`
---
+-- AUTO_INCREMENT para tabela `tb_produtos`
 ALTER TABLE `tb_produtos`
-  MODIFY `produto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `produto_id` int(11) NOT NULL AUTO_INCREMENT;
