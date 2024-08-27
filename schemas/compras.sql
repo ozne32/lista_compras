858db75fe14ebd1562e53f28791d7c0a766751bf
ozne32 <enzorcc.mf3rs@gmail.com>
2024-08-26 21:43:57 -0300

    ajustando a tabela

--- schemas/compras.sql
+++ schemas/compras.sql
@@ -1,22 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Tempo de geração: 27/08/2024 às 00:10
+-- Versão do servidor: 11.4.2-MariaDB-ubu2404
+-- Versão do PHP: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Banco de dados: `compras`
-
--- --------------------------------------------------------
-
 --
--- Estrutura para tabela `tb_user_prods`
---
-
-DROP TABLE IF EXISTS `tb_user_prods`;
-
-CREATE TABLE `tb_user_prods` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_prods` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_user_prod` (`id_prods`, `id_user`),
-  CONSTRAINT `tb_user_prods_ibfk_1` FOREIGN KEY (`id_prods`) REFERENCES `tb_produtos` (`produto_id`),
-  CONSTRAINT `tb_user_prods_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_usuarios` (`usuario_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -24,14 +27,23 @@ CREATE TABLE `tb_user_prods` (
 -- Estrutura para tabela `tb_produtos`
 --
 
-DROP TABLE IF EXISTS `tb_produtos`;
-
 CREATE TABLE `tb_produtos` (
-  `produto_id` int(11) NOT NULL AUTO_INCREMENT,
+  `produto_id` int(11) NOT NULL,
   `nome_produto` varchar(255) NOT NULL,
-  `comprado` int(2) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`produto_id`)
+  `comprado` int(2) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_user_prods`
+--
+
+CREATE TABLE `tb_user_prods` (
+  `id_prods` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
 
 -- --------------------------------------------------------
 
@@ -39,14 +51,65 @@ CREATE TABLE `tb_produtos` (
 -- Estrutura para tabela `tb_usuarios`
 --
 
-DROP TABLE IF EXISTS `tb_usuarios`;
-
 CREATE TABLE `tb_usuarios` (
-  `usuario_id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
-  `senha` varchar(100) NOT NULL,
-  PRIMARY KEY (`usuario_id`)
+  `senha` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- --------------------------------------------------------
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  ADD PRIMARY KEY (`produto_id`);
+
+--
+-- Índices de tabela `tb_user_prods`
+--
+ALTER TABLE `tb_user_prods`
+  ADD KEY `id_prods` (`id_prods`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Índices de tabela `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD PRIMARY KEY (`usuario_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  MODIFY `produto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+
+--
+-- AUTO_INCREMENT de tabela `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `tb_user_prods`
+--
+ALTER TABLE `tb_user_prods`
+  ADD CONSTRAINT `tb_user_prods_ibfk_1` FOREIGN KEY (`id_prods`) REFERENCES `tb_produtos` (`produto_id`),
+  ADD CONSTRAINT `tb_user_prods_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_usuarios` (`usuario_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
