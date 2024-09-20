d963adb5e994694190f371ccf2ed5b7bd3cf74c9
ozne32 <enzorcc.mf3rs@gmail.com>
2024-09-20 17:12:48 -0300

    .

--- comeco/controller.php
+++ comeco/controller.php
@@ -620,8 +620,8 @@ if ($acao == 'novaSenha') {
                 $mail->isSMTP();                                          //Send using SMTP
                 $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-                $mail->Username = 'emailgenerico';                     //SMTP username
-                $mail->Password = 'senha';                               //SMTP password
+                $mail->Username =                      //SMTP username --> email
+                $mail->Password =                                //SMTP password -->senha
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
                 $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
