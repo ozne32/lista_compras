d9a603f513b5ef1a56f8b91e41f96185e41c7e00
ozne32 <enzorcc.mf3rs@gmail.com>
2024-09-20 15:53:21 -0300

    melhorando a lógica do adicionar usuário

--- comeco/controller.php
+++ comeco/controller.php
@@ -620,8 +620,8 @@ if ($acao == 'novaSenha') {
                 $mail->isSMTP();                                          //Send using SMTP
                 $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-                $mail->Username = 'enzorcc.mf3rs@gmail.com';                     //SMTP username
-                $mail->Password = 'ijrl cwda fczs bgle';                               //SMTP password
+                $mail->Username = 'emailgenerico';                     //SMTP username
+                $mail->Password = 'senha';                               //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
                 $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
